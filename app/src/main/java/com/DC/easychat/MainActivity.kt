115945e0d218cae5627b81b1d0f7355b0f26681b
Daniel Coria <daniel.coria@accedo.tv>
2019-08-02 18:21:45 -0500

    new login activity

--- app/src/main/java/com/DC/easychat/MainActivity.kt
+++ app/src/main/java/com/DC/easychat/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.DC.easychat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loginBtn.setOnClickListener{
+            println("test")
+            val intent = Intent(this, LoginInActivity:: class.java)
+            this.startActivity(intent)
+
+        }
+
 
 
 
